5c148319c22c20c1b439465eeaea6f1df583d6f5
g67277 <nshuqair@fullsail.edu>
2013-10-24 00:14:38 -0400

    almost done with gold app

---
+++ Shuqair_Nazir_Project4/Gold/js/main.js
@@ -0,0 +1,59 @@
+// Nazir Shuqair
+//	MiU 1310
+//	17 OCT 2013
+//	Project 3
+
+$('#home').on('pageinit', function(){
+	//code needed for home page goes here
+});	
+		
+$('#additem').on('pageinit', function(){
+
+		var myForm = $('#vtc');
+		myForm.validate({
+		invalidHandler: function(form, validator) {
+		},
+		submitHandler: function() {
+			var data = myForm.serializeArray();
+			storeData(data);
+			//reloads page to clear fields
+			window.location.reload(true);
+		}
+});
+	
+	//any other code needed for addItem page goes here
+	var storeData = function(data){
+		
+		//assigns id to the item
+		var id 				= Math.floor(Math.random() * 100001);
+		
+		localStorage.setItem(id, JSON.stringify(data));
+		console.log(data);
+		alert("Meeting Saved!");
+	}
+	
+});
+
+//The functions below can go inside or outside the pageinit function for the page in which it is needed.
+
+var autofillData = function (){
+	 
+};
+
+var getData = function(){
+
+};
+
+var storeData = function(data){
+	
+}; 
+
+var	deleteItem = function (){
+			
+};
+					
+var clearLocal = function(){
+
+};
+
+
